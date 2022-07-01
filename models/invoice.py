bff1dbe25eb6c29903ae318f845f2924fcbd8990
vanevb <vanevb976@gmail.com>
2022-06-30 21:04:05 -0500

    CRUD invoice

--- models/invoice.py
+++ models/invoice.py
@@ -3,12 +3,52 @@
 Invoce model
 """
 
+from mysql.connector import Error
+from database.connection import DAO
 
-class Invoice():
+
+class Invoice(DAO):
     """ Invoice class """
+    
+    def __init__(self):
+        """ Inherit connection """
+        super().__init__()
 
-    def __init__(self, client_id: int, product_id: int, quantity: int, cost: int):
-        self.client_id = client_id
-        self.product_id = product_id
-        self.quantity = quantity
-        self.cost = cost
+    def get_all_invoices(self):
+        """ Get all invoicing"""
+        if self.connection.is_connected():
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute("SELECT * from invoice")
+                invoice_result = cursor.fetchall()
+                return invoice_result
+            except Error as e:
+                print(f"An error occurred while retrieve data: {e}")
+                
+    def create_new_invoice(self, client_id: int, product_id: int, quantity: int, total_value: int):
+        """ Create new invoice"""
+        if self.connection.is_connected():
+            try:
+                cursor = self.connection.cursor()
+                sql = f"INSERT INTO invoice (client_id, product_id, quantity, total_value) VALUES ('{client_id}', '{product_id}', '{quantity}', {total_value})"
+                cursor.execute(sql)
+                self.connection.commit()
+                print("\n=> Invoice created successfully")
+            except Error as e:
+                print(f"An error occurred while creating the invoice: {e}")
+    
+    def delete_invoice(self, id: int):
+        """ Delete invoice by id """
+        if self.connection.is_connected():
+            try:
+                cursor = self.connection.cursor()
+                sql = f"DELETE FROM invoice WHERE id = '{id}'"
+                cursor.execute(sql)
+                self.connection.commit()
+                print("\n=> Invoice deleted successfully")
+            except Error as e:
+                print(f"An error occurred while deleting the invoice: {e}")
+    
+
+
+
